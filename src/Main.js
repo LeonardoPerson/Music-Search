034850aba713f1fe246951146c6f283e34af1424
Leonardo Muros do Nascimento <lmuros@gmail.com>
2020-11-27 09:57:49 -0300

    Creating the form with the field and search button

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,29 @@
+import {useState} from 'react'
 
+const apiURL = `https://api.lyrics.ovh/`
+const applyCors = 'https://cors-anywhere.herokuapp.com/'
+const imageAlternative = 'https://askleo.askleomedia.com/wp-content/uploads/2004/06/no_image-300x245.jpg'
 
 const Main = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [termWarning, setTermWarning] = useState(false)
+  const [musicData, setMusicData] = useState([])
+  const [error, setError] = useState(null)
+
   return(
-    <div>Main</div>
+    <div>
+      <h1 id='appTitle'>Music Search</h1>
+      
+      <form /*onSubmit={searchLyric}*/ autoComplete="on">
+        <input
+          autoFocus
+          type='text'
+          /*onChange={handleChange}*/
+          placeholder='Insira o nome do artista ou da música...'
+        />
+        <button>Search</button>
+      </form>     
+    </div>
   )
 }
 
