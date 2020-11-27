0b2b5836ed14de9a3764d27d29c93d07366bc40d
Leonardo Muros do Nascimento <lmuros@gmail.com>
2020-11-27 09:45:41 -0300

    Creating the initial structure of the App.js file with the Header.js, Main.js and Footer.js components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
 
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      App
+    <div className='App'>
+      <div className='header-container'>
+        <Header />
+      </div>
+      <div className='main-container'>
+        <Main />
+      </div>
+      <div className='footer-container'>
+        <Footer />
+      </div>
     </div>
   );
 }
